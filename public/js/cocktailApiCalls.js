4d676bfdd83ba2347361bab49f7d1f076b8d4d69
jproff <jessicakproffitt@gmail.com>
2023-12-05 18:15:33 -0500

    returning data but ingredient

--- public/js/cocktailApiCalls.js
+++ public/js/cocktailApiCalls.js
@@ -1,5 +1,3 @@
-//enter all js files here
-
 const imgEl = document.getElementById('thumbnailImg');
 const drinkEl = document.getElementById('cocktail-name');
 const drinkImg = document.getElementById('drinkImg');
@@ -30,6 +28,17 @@ const newExploreFormHandler = async (event) => {
 
 function buildCocktailPage(data) {
   console.log('This is the builder function, here is the related data: ', data);
+  ingredientKeys = Object.keys(data)
+    .filter((key) => key.includes('Ingredient'))
+    .reduce((obj, key) => {
+      obj[key] = data[key];
+      return obj;
+    }, {});
+  console.log(ingredientKeys);
+  let ingredientList = Object.fromEntries(
+    Object.entries(ingredientKeys).filter(([_, v]) => v != null)
+  );
+  console.log(ingredientList);
   const cpTitle = document.getElementById('title');
   const cpDrinkType = document.getElementById('drinkType');
   const cpIngredients = document.getElementById('ingredients');
@@ -37,6 +46,10 @@ function buildCocktailPage(data) {
   const cpGlass = document.getElementById('glassware');
 
   cpDrinkType.src = data.strDrinkThumb;
+  cpTitle.textContent = data.strDrink;
+  cpGlass.textContent = data.strGlass;
+  cpIngredients.textContent = ingredientList.json();
+  cpInstructions.textContent = data.strInstructions;
 }
 //event listener for homepage
 document
@@ -60,28 +73,6 @@ if (!drinkEl) {
   });
 }
 
-//look up cocktail by ID
-const getDrinkById = async (id) => {
-  const response = await fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-  );
-  const data = await response.json();
-  return data.drinks;
-};
-
-// getDrinkById('11007').then((data) => console.log(data));
-
-//search for ingredient by name
-const getIngredientByName = async (name) => {
-  const response = await fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${name}`
-  );
-  const data = await response.json();
-  return data.ingredients;
-};
-
-// getIngredientByName('lemon').then((data) => console.log(data));
-
 //get random cocktail
 const getRandomDrink = async () => {
   const response = await fetch(
@@ -90,6 +81,24 @@ const getRandomDrink = async () => {
   const data = await response.json();
   return data.drinks;
 };
+
+//look up cocktail by ID
+// const getDrinkById = async (id) => {
+//   const response = await fetch(
+//     `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+//   );
+//   const data = await response.json();
+//   return data.drinks;
+// };
+
+//search for ingredient by name
+// const getIngredientByName = async (name) => {
+//   const response = await fetch(
+//     `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${name}`
+//   );
+//   const data = await response.json();
+//   return data.ingredients;
+// };
 
 getRandomDrink().then((data) => {
   imgEl.src = data[0].strDrinkThumb;
@@ -111,34 +120,28 @@ if (searchForm) {
   });
 }
 
-const displayCocktailDetails = async (cocktailName) => {
-  const drinks = await getDrinkByName(cocktailName);
-  if (drinks && drinks.length > 0) {
-    const cocktail = drinks[0];
-    // Assuming ingredients are stored in strIngredient1, strIngredient2, ...
-    const ingredients = [];
-    for (let i = 1; i <= 15; i++) {
-      const ingredient = cocktail[`strIngredient${i}`];
-      if (ingredient) {
-        ingredients.push(ingredient);
-      } else {
-        break;
-      }
-    }
+// const displayCocktailDetails = async (cocktailName) => {
+//   const drinks = await getDrinkByName(cocktailName);
+//   if (drinks && drinks.length > 0) {
+//     const cocktail = drinks[0];
+//     // Assuming ingredients are stored in strIngredient1, strIngredient2, ...
+//     const ingredients = [];
+//     for (let i = 1; i <= 15; i++) {
+//       const ingredient = cocktail[`strIngredient${i}`];
+//       if (ingredient) {
+//         ingredients.push(ingredient);
+//       } else {
+//         break;
+//       }
+//     }
 
-    // Redirect to cocktailDisplay page with details
-    window.location.href = `/cocktailDisplay?name=${encodeURIComponent(
-      cocktailName
-    )}&ingredients=${encodeURIComponent(
-      JSON.stringify(ingredients)
-    )}&instructions=${encodeURIComponent(
-      cocktail.strInstructions
-    )}&image=${encodeURIComponent(cocktail.strDrinkThumb)}`;
-  }
-};
-
-function temp(value) {
-  console.log('jere');
-  console.log(value);
-  console.log(searchInput);
-}
+//     // Redirect to cocktailDisplay page with details
+//     window.location.href = `/cocktailDisplay?name=${encodeURIComponent(
+//       cocktailName
+//     )}&ingredients=${encodeURIComponent(
+//       JSON.stringify(ingredients)
+//     )}&instructions=${encodeURIComponent(
+//       cocktail.strInstructions
+//     )}&image=${encodeURIComponent(cocktail.strDrinkThumb)}`;
+//   }
+// };
