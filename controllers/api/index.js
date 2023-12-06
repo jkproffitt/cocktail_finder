5b94323c602644c241fe0360cd06aa22487e4df9
jproff <jessicakproffitt@gmail.com>
2023-12-05 20:29:47 -0500

    prettier

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const cocktailRoutes = require('./cocktailRoutes');
-const favDrinkRoutes = require('./favDrinkRoutes')
 
 router.use('/users', userRoutes);
 router.use('/cocktail', cocktailRoutes);
-router.use('/favDrink', favDrinkRoutes);
 
 const loginRoute = require('./loginRoute');
 const logoutRoute = require('./logoutRoute');
--- public/js/age.js
+++ public/js/age.js
@@ -1,32 +1,32 @@
-// const ageHandler = async (e) => {
-//   e.preventDefault();
-//   const dob = document.querySelector('#birthdate').value.trim();
-//   console.log(dob);
-//   var age = getAge(dob);
-//   console.log(age);
-//   if (dob == '' || age < 21) {
-//     $('#birthdate').attr(
-//       'style',
-//       'background-color: rgb(255, 245, 235); border-color: red; '
-//     );
-//   }
-//   if (age < 21) {
-//     document.querySelector('#submit-btn').disabled = true;
-//   } else {
-//     document.querySelector('#submit-btn').disabled = false;
-//   }
-// };
+const ageHandler = async (e) => {
+  e.preventDefault();
+  const dob = document.querySelector('#birthdate').value.trim();
+  console.log(dob);
+  var age = getAge(dob);
+  console.log(age);
+  if (dob == '' || age < 21) {
+    $('#birthdate').attr(
+      'style',
+      'background-color: rgb(255, 245, 235); border-color: red; '
+    );
+  }
+  if (age < 21) {
+    document.querySelector('#submit-btn').disabled = true;
+  } else {
+    document.querySelector('#submit-btn').disabled = false;
+  }
+};
 
-// function getAge(dob) {
-//   var today = new Date(),
-//     birthDate = new Date(dob),
-//     age = today.getFullYear() - birthDate.getFullYear(),
-//     m = today.getMonth() - birthDate.getMonth();
-//   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-//     age--;
-//   }
-//   return age;
-// }
+function getAge(dob) {
+  var today = new Date(),
+    birthDate = new Date(dob),
+    age = today.getFullYear() - birthDate.getFullYear(),
+    m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
 
 // // const rememberSession = async (e) => {
 // //   e.preventDefault();
